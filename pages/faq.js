b40bb7b9664837612ca13594ee90971997b789cb
Harry-Yates <hyates1@gmail.com>
2023-01-31 14:40:18 +0100

    password bug fix (working)

--- pages/faq.js
+++ pages/faq.js
@@ -2,9 +2,8 @@ import Head from "next/head";
 import { useRouter } from "next/router";
 import Login from "@/components/shared/Login";
 import Header from "@/components/shared/Header";
-import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
-const FAQ = ({ hasReadPermission }) => {
+export default function FAQ({ hasReadPermission }) {
   const router = useRouter();
 
   if (!hasReadPermission) {
@@ -16,20 +15,11 @@ const FAQ = ({ hasReadPermission }) => {
       <Head>
         <title>FAQ Page</title>
       </Head>
+
       <main>
-        <h2>{t("hello world")}</h2>
-        <h1>FAQ Page</h1> <Header />
+        <h1>FAQ Page</h1>
+        <Header />
       </main>
     </div>
   );
-};
-
-export async function getStaticProps({ locale }) {
-  return {
-    props: {
-      ...(await serverSideTranslations(locale, ["copy"]))
-    }
-  };
 }
-
-export default FAQ;
