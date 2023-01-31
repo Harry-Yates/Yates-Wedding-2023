bbaa1f370e76f1642e31885a5bf96bb29479c742
Harry-Yates <hyates1@gmail.com>
2023-01-31 16:31:59 +0100

    adding more stuff

--- pages/rspv.js
+++ pages/rspv.js
@@ -2,13 +2,9 @@ import Head from "next/head";
 import { useRouter } from "next/router";
 import Login from "@/components/shared/Login";
 import Header from "@/components/shared/Header";
-import { useTranslation } from "next-i18next";
-import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
-const RSPV = ({ hasReadPermission }) => {
+export default function RSVP({ hasReadPermission }) {
   const router = useRouter();
-
-  const { t } = useTranslation("copy");
 
   if (!hasReadPermission) {
     return <Login redirectPath={router.asPath} />;
@@ -17,23 +13,12 @@ const RSPV = ({ hasReadPermission }) => {
   return (
     <div>
       <Head>
-        <title>RSPV Page</title>
+        <title>RSVP Page</title>
       </Head>
 
       <main>
-        <h2>{t("hello world")}</h2>
-        <h1>RSPV Page</h1> <Header />
+        <h1>RSVP Page</h1> <Header />
       </main>
     </div>
   );
-};
-
-export async function getStaticProps({ locale }) {
-  return {
-    props: {
-      ...(await serverSideTranslations(locale, ["copy"]))
-    }
-  };
 }
-
-export default RSPV;
