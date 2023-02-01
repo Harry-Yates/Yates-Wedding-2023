b5f4c481d1c4a5f423097bb78a8082edbafe2a6f
Harry-Yates <hyates1@gmail.com>
2023-02-01 10:39:12 +0100

    reset

--- components/shared/Header.js
+++ components/shared/Header.js
@@ -1,21 +1,8 @@
 import Link from "next/link";
-import { useRouter } from "next/router";
 
 export default function Header() {
-  const { locale, locales, push, pathname } = useRouter();
-
-  const handleClick = (l) => () => {
-    push(pathname, undefined, { locale: l });
-  };
-
   return (
     <>
-      <h1>{locale}</h1>
-      {/* {locales.map((l) => (
-        <button key={l} onClick={handleClick(l)}>
-          {l}
-        </button>
-      ))} */}
       <Link href='/'>Home</Link>
       <Link href='/faq'>faq</Link>
       <Link href='/rspv'>rspv</Link>
