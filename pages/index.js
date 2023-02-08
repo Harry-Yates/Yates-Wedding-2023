60dd6a32836693af70a00d43b75780ac444814ef
Harry-Yates <hyates1@gmail.com>
2023-02-08 14:45:50 +0000

    got started with the slider

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,47 @@ import Login from "@/components/shared/Login";
 import { enGB, svSE, itIT } from "@/translations";
 import BaseLayout from "../components/layouts/BaseLayout";
 import BasePage from "../components/BasePage";
+import "keen-slider/keen-slider.min.css";
+import { useKeenSlider } from "keen-slider/react"; // import from 'keen-slider/react.es' for to get an ES module
 
 export default function Home({ hasReadPermission }) {
   const router = useRouter();
   const { locale } = router;
+  const [sliderRef] = useKeenSlider(
+    {
+      loop: true
+    },
+    [
+      (slider) => {
+        let timeout;
+        let mouseOver = false;
+        function clearNextTimeout() {
+          clearTimeout(timeout);
+        }
+        function nextTimeout() {
+          clearTimeout(timeout);
+          if (mouseOver) return;
+          timeout = setTimeout(() => {
+            slider.next();
+          }, 4000);
+        }
+        slider.on("created", () => {
+          slider.container.addEventListener("mouseover", () => {
+            mouseOver = true;
+            clearNextTimeout();
+          });
+          slider.container.addEventListener("mouseout", () => {
+            mouseOver = false;
+            nextTimeout();
+          });
+          nextTimeout();
+        });
+        slider.on("dragStarted", clearNextTimeout);
+        slider.on("animationEnded", nextTimeout);
+        slider.on("updated", nextTimeout);
+      }
+    ]
+  );
 
   const t = locale === "en-GB" ? enGB : locale === "sv-SE" ? svSE : itIT;
 
@@ -27,6 +64,14 @@ export default function Home({ hasReadPermission }) {
           <option value='it-IT'>IT 🇮🇹</option>
         </select>
         <h1>{t.home_title}</h1>
+        <div ref={sliderRef} className='keen-slider'>
+          <div className='keen-slider__slide number-slide1'>1</div>
+          <div className='keen-slider__slide number-slide2'>2</div>
+          <div className='keen-slider__slide number-slide3'>3</div>
+          <div className='keen-slider__slide number-slide4'>4</div>
+          <div className='keen-slider__slide number-slide5'>5</div>
+          <div className='keen-slider__slide number-slide6'>6</div>
+        </div>
       </BasePage>
     </BaseLayout>
   );
