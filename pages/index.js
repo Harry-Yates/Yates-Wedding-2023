7a2f34966abe0a2729c622ad712a6338abe7efe8
Harry-Yates <hyates1@gmail.com>
2023-02-14 12:12:01 +0000

    added a favicon and meta data

--- pages/index.js
+++ pages/index.js
@@ -91,6 +91,13 @@ export default function Home({ hasReadPermission }) {
     <BaseLayout>
       <BasePage>
         <Head>
+          <link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />
+          <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
+          <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
+          <link rel='manifest' href='/site.webmanifest' />
+          <link rel='mask-icon' href='/safari-pinned-tab.svg' color='#5bbad5' />
+          <meta name='msapplication-TileColor' content='#da532c' />
+          <meta name='theme-color' content='#ffffff'></meta>
           <title>Yates Wedding 2023</title>
           <meta name='title' content='Yates Wedding 2023' />
           <meta name='description' content=' 8th-9th September | Colognola di Casazza, Italy' />
