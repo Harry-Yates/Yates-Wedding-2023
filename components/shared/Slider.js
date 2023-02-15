59b332b7b4cf73ff7a1a95c0046fd00a53735a05
Harry-Yates <hyates1@gmail.com>
2023-02-15 18:31:19 +0000

    tidy up

--- components/shared/Slider.js
+++ components/shared/Slider.js
@@ -1,10 +1,9 @@
-import React from 'react'
+import React from "react";
 import "keen-slider/keen-slider.min.css";
 import { useKeenSlider } from "keen-slider/react";
 import Image from "next/image";
 
 //! IMAGES
-import flowersMain from "@/public/decoration/flowers-main.png.webp";
 import leaving from "@/public/slider/leaving.png";
 import arholma from "@/public/slider/arholma.png";
 import sunset from "@/public/slider/sunset.png";
@@ -23,167 +22,166 @@ import hyde from "@/public/slider/hyde.png";
 import henge from "@/public/slider/henge.png";
 
 const Slider = () => {
-
-	const [sliderRef] = useKeenSlider(
-		{
-		  loop: true
-		},
-		[
-		  (slider) => {
-			let timeout;
-			let mouseOver = false;
-			function clearNextTimeout() {
-			  clearTimeout(timeout);
-			}
-			function nextTimeout() {
-			  clearTimeout(timeout);
-			  if (mouseOver) return;
-			  timeout = setTimeout(() => {
-				slider.next();
-			  }, 4000);
-			}
-			slider.on("created", () => {
-			  slider.container.addEventListener("mouseover", () => {
-				mouseOver = true;
-				clearNextTimeout();
-			  });
-			  slider.container.addEventListener("mouseout", () => {
-				mouseOver = false;
-				nextTimeout();
-			  });
-			  nextTimeout();
-			});
-			slider.on("dragStarted", clearNextTimeout);
-			slider.on("animationEnded", nextTimeout);
-			slider.on("updated", nextTimeout);
-		  }
-		]
-	  );
-
+  const [sliderRef] = useKeenSlider(
+    {
+      loop: true
+    },
+    [
+      (slider) => {
+        let timeout;
+        let mouseOver = false;
+        function clearNextTimeout() {
+          clearTimeout(timeout);
+        }
+        function nextTimeout() {
+          clearTimeout(timeout);
+          if (mouseOver) return;
+          timeout = setTimeout(() => {
+            slider.next();
+          }, 4000);
+        }
+        slider.on("created", () => {
+          slider.container.addEventListener("mouseover", () => {
+            mouseOver = true;
+            clearNextTimeout();
+          });
+          slider.container.addEventListener("mouseout", () => {
+            mouseOver = false;
+            nextTimeout();
+          });
+          nextTimeout();
+        });
+        slider.on("dragStarted", clearNextTimeout);
+        slider.on("animationEnded", nextTimeout);
+        slider.on("updated", nextTimeout);
+      }
+    ]
+  );
 
   return (
-	<div ref={sliderRef} className='keen-slider'>
-	<Image
-	  src={mountain}
-	  alt='mountain'
-	  className='keen-slider__slide number-slidex'
-	  height={400}
-	  width={1000}
-	/>
-	<Image
-	  src={walks2}
-	  alt='walks2'
-	  className='keen-slider__slide number-slidex'
-	  height={400}
-	  width={1000}
-	/>
-	<Image
-	  src={sunset}
-	  alt='sunset'
-	  className='keen-slider__slide number-slidex'
-	  height={400}
-	  width={1000}
-	/>
-	<Image
-	  src={chloeplace}
-	  alt='chloeplace'
-	  className='keen-slider__slide number-slidex'
-	  height={400}
-	  width={1000}
-	/>
-	<Image
-	  src={henge}
-	  alt='henge'
-	  className='keen-slider__slide number-slidex'
-	  height={400}
-	  width={1000}
-	/>
-	<Image
-	  src={hyde}
-	  alt='hyde'
-	  className='keen-slider__slide number-slidex'
-	  height={400}
-	  width={1000}
-	/>
-	<Image
-	  src={climbers}
-	  alt='climbers'
-	  className='keen-slider__slide number-slidex'
-	  height={400}
-	  width={1000}
-	/>
-	<Image
-	  src={mountain}
-	  alt='mountain'
-	  className='keen-slider__slide number-slidex'
-	  height={400}
-	  width={1000}
-	/>
-	<Image
-	  src={water}
-	  alt='water'
-	  className='keen-slider__slide number-slidex'
-	  height={400}
-	  width={1000}
-	/>
-	<Image
-	  src={arholma}
-	  alt='arholma'
-	  className='keen-slider__slide number-slidex'
-	  height={400}
-	  width={1000}
-	/>
-	<Image
-	  src={austria}
-	  alt='austria'
-	  className='keen-slider__slide number-slidex'
-	  height={400}
-	  width={1000}
-	/>
-	<Image
-	  src={jacket}
-	  alt='jacket'
-	  className='keen-slider__slide number-slidex'
-	  height={400}
-	  width={1000}
-	/>
-	<Image
-	  src={leaving}
-	  alt='leaving'
-	  className='keen-slider__slide number-slidex'
-	  height={400}
-	  width={1000}
-	/>
-	<Image
-	  src={walks}
-	  alt='walks'
-	  className='keen-slider__slide number-slidex'
-	  height={400}
-	  width={1000}
-	/>
-	<Image
-	  src={venice}
-	  alt='venice'
-	  className='keen-slider__slide number-slidex'
-	  height={400}
-	  width={1000}
-	/>
-	<Image
-	  src={whisky}
-	  alt='whisky'
-	  className='keen-slider__slide number-slidex'
-	  height={400}
-	  width={1000}
-	/>
-	<Image
-	  src={smart}
-	  alt='smart'
-	  className='keen-slider__slide number-slidex'
-	  height={400}
-	  width={1000}
-	/>
-  </div>
-  )
-}
+    <div ref={sliderRef} className='keen-slider'>
+      <Image
+        src={mountain}
+        alt='mountain'
+        className='keen-slider__slide number-slidex'
+        height={400}
+        width={1000}
+        priority
+      />
+      <Image
+        src={walks2}
+        alt='walks2'
+        className='keen-slider__slide number-slidex'
+        height={400}
+        width={1000}
+      />
+      <Image
+        src={sunset}
+        alt='sunset'
+        className='keen-slider__slide number-slidex'
+        height={400}
+        width={1000}
+      />
+      <Image
+        src={chloeplace}
+        alt='chloeplace'
+        className='keen-slider__slide number-slidex'
+        height={400}
+        width={1000}
+      />
+      <Image
+        src={henge}
+        alt='henge'
+        className='keen-slider__slide number-slidex'
+        height={400}
+        width={1000}
+      />
+      <Image
+        src={hyde}
+        alt='hyde'
+        className='keen-slider__slide number-slidex'
+        height={400}
+        width={1000}
+      />
+      <Image
+        src={climbers}
+        alt='climbers'
+        className='keen-slider__slide number-slidex'
+        height={400}
+        width={1000}
+      />
+      <Image
+        src={mountain}
+        alt='mountain'
+        className='keen-slider__slide number-slidex'
+        height={400}
+        width={1000}
+      />
+      <Image
+        src={water}
+        alt='water'
+        className='keen-slider__slide number-slidex'
+        height={400}
+        width={1000}
+      />
+      <Image
+        src={arholma}
+        alt='arholma'
+        className='keen-slider__slide number-slidex'
+        height={400}
+        width={1000}
+      />
+      <Image
+        src={austria}
+        alt='austria'
+        className='keen-slider__slide number-slidex'
+        height={400}
+        width={1000}
+      />
+      <Image
+        src={jacket}
+        alt='jacket'
+        className='keen-slider__slide number-slidex'
+        height={400}
+        width={1000}
+      />
+      <Image
+        src={leaving}
+        alt='leaving'
+        className='keen-slider__slide number-slidex'
+        height={400}
+        width={1000}
+      />
+      <Image
+        src={walks}
+        alt='walks'
+        className='keen-slider__slide number-slidex'
+        height={400}
+        width={1000}
+      />
+      <Image
+        src={venice}
+        alt='venice'
+        className='keen-slider__slide number-slidex'
+        height={400}
+        width={1000}
+      />
+      <Image
+        src={whisky}
+        alt='whisky'
+        className='keen-slider__slide number-slidex'
+        height={400}
+        width={1000}
+      />
+      <Image
+        src={smart}
+        alt='smart'
+        className='keen-slider__slide number-slidex'
+        height={400}
+        width={1000}
+      />
+    </div>
+  );
+};
 
-export default Slider
+export default Slider;
