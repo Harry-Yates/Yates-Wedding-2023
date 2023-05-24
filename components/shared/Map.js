8343364ae95b308c2d0b68ca7d81b6b7e7c11eb3
Harry-Yates <hyates1@gmail.com>
2023-05-24 15:04:23 +0200

    imporved the map

--- components/shared/Map.js
+++ components/shared/Map.js
@@ -1,4 +1,10 @@
-import { GoogleMap, useLoadScript, MarkerF } from "@react-google-maps/api";
+import React from "react";
+import {
+  GoogleMap,
+  useLoadScript,
+  MarkerF,
+  InfoWindow,
+} from "@react-google-maps/api";
 import { styles } from "@/styles/componentStyles/mapStyles";
 
 const libraries = ["places"];
@@ -13,86 +19,73 @@ const options = {
   zoomControl: true,
 };
 
-const center = [
+const center = {
+  lat: 45.750038,
+  lng: 9.90993,
+};
+
+const markers = [
   {
-    lat: 45.750038,
-    lng: 9.90993,
+    id: "Church",
+    position: { lat: 45.7644535548191, lng: 9.922673633853233 },
+    icon: "https://user-images.githubusercontent.com/82885837/218527014-641fa8a9-6f0e-4e1a-9dec-b879a6096515.png",
+  },
+  {
+    id: "Casa Lodi-Yates",
+    position: { lat: 45.745911, lng: 9.91433 },
+    icon: "https://user-images.githubusercontent.com/82885837/218531320-8c8c9c29-f96a-46d4-90c2-2122f8cb7524.png",
+  },
+  {
+    id: "Vulcano Village",
+    position: { lat: 45.82195, lng: 10.08195 },
+    icon: "https://user-images.githubusercontent.com/82885837/218538598-388ec01e-809e-42a9-b6a9-491f47324ab5.png",
+  },
+  {
+    id: "Hotel Lovere Resort & Spa",
+    position: { lat: 45.8025134, lng: 10.0656494 },
+    icon: "https://user-images.githubusercontent.com/82885837/218538598-388ec01e-809e-42a9-b6a9-491f47324ab5.png",
+  },
+  {
+    id: "Locanda del Boscaiolo",
+    position: { lat: 45.76089607418838, lng: 9.92914617061615 },
+    icon: "https://user-images.githubusercontent.com/82885837/218538598-388ec01e-809e-42a9-b6a9-491f47324ab5.png",
   },
 ];
+
+// Function to adjust the position of the InfoWindow
+const adjustPosition = (position) => {
+  return {
+    lat: position.lat + 0.01, // Adjust this value to move the InfoWindow
+    lng: position.lng,
+  };
+};
 
 const Map = () => {
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+    libraries: libraries,
   });
 
   if (!isLoaded) return "loading Maps...";
 
   return (
-    isLoaded && (
-      <>
-        {/* <div className='map-container'> */}
-        <GoogleMap
-          mapContainerStyle={mapContainerStyle}
-          zoom={12}
-          center={{ lat: 45.750038, lng: 9.90993 }}
-          options={options}>
-          {/* Casazza Marker */}
+    <GoogleMap
+      mapContainerStyle={mapContainerStyle}
+      zoom={11}
+      center={center}
+      options={options}>
+      {markers.map((marker, index) => (
+        <React.Fragment key={marker.id}>
           <MarkerF
-            id="marker"
-            position={{ lat: 45.750038, lng: 9.90993 }}
-            // icon={{
-            //   url: "https://user-images.githubusercontent.com/82885837/218527014-641fa8a9-6f0e-4e1a-9dec-b879a6096515.png"
-            // }}
+            position={marker.position}
+            icon={marker.icon && { url: marker.icon }}
           />
-
-          {/* Chiesa di San Pietro in vincoli, Via San Pietro, 24060 Spinone al Lago BG, Italy */}
-          <MarkerF
-            id="church"
-            position={{ lat: 45.7644535548191, lng: 9.922673633853233 }}
-            icon={{
-              url: "https://user-images.githubusercontent.com/82885837/218527014-641fa8a9-6f0e-4e1a-9dec-b879a6096515.png",
-            }}
-          />
-
-          {/* Piazza Armando Diaz, 1, Casazza, BG */}
-          <MarkerF
-            id="casa lodi-yates"
-            position={{ lat: 45.82195, lng: 10.08195 }}
-            icon={{
-              url: "https://user-images.githubusercontent.com/82885837/218531320-8c8c9c29-f96a-46d4-90c2-2122f8cb7524.png",
-            }}
-          />
-
-          {/* HOTEL LOVERE RESORT AND SPAR */}
-          <MarkerF
-            id="Hotel 1"
-            position={{ lat: 45.6900668, lng: 9.846434 }}
-            icon={{
-              url: "https://user-images.githubusercontent.com/82885837/218538598-388ec01e-809e-42a9-b6a9-491f47324ab5.png",
-            }}
-          />
-
-          {/* VULCANO VILLAGE  */}
-          <MarkerF
-            id="Hotel 1"
-            position={{ lat: 45.8025134, lng: 10.0656494 }}
-            icon={{
-              url: "https://user-images.githubusercontent.com/82885837/218538598-388ec01e-809e-42a9-b6a9-491f47324ab5.png",
-            }}
-          />
-
-          {/* LOCANDA DEL BOSCAIOLO */}
-          <MarkerF
-            id="Hotel 1"
-            position={{ lat: 45.76089607418838, lng: 9.92914617061615 }}
-            icon={{
-              url: "https://user-images.githubusercontent.com/82885837/218538598-388ec01e-809e-42a9-b6a9-491f47324ab5.png",
-            }}
-          />
-        </GoogleMap>
-      </>
-      // </div>
-    )
+          <InfoWindow position={adjustPosition(marker.position)}>
+            <div>{marker.id}</div>
+          </InfoWindow>
+        </React.Fragment>
+      ))}
+    </GoogleMap>
   );
 };
 
